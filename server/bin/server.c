3ed34cba4719a2a1f19d687694632fe2a4601ec7
Stuart Terrett <shterrett@gmail.com>
2016-01-06 07:36:09 -0500

    Try to ignore only binaries

---
+++ server/bin/server.c
@@ -0,0 +1,49 @@
+#include <dbg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[])
+{
+  int sockfd, newsockfd, portno;
+  socklen_t clilen;
+  char buffer[256];
+  struct sockaddr_in serv_addr, cli_addr;
+  int n;
+  if (argc < 2) {
+    fprintf(stderr,"ERROR, no port provided\n");
+    exit(1);
+  }
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  check(sockfd >= 0, "error opening socket");
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  portno = atoi(argv[1]);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+  check(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) >= 0,
+        "ERROR on binding"
+       );
+  listen(sockfd,5);
+  clilen = sizeof(cli_addr);
+  newsockfd = accept(sockfd,
+      (struct sockaddr *) &cli_addr,
+      &clilen);
+  check(newsockfd >= 0, "error on accept");
+  bzero(buffer,256);
+  n = read(newsockfd,buffer,255);
+  check(n >= 0, "error reading from socket");
+  log_info("Here is the message: %s",buffer);
+  n = write(newsockfd,"I got your message",18);
+  check(n >= 0, "Error writing to socket");
+  close(newsockfd);
+  close(sockfd);
+  return 0;
+
+error:
+  return -1;
+}
